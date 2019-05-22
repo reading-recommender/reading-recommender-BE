5cc41a03a3729f78cde24975fa7ea184804faeb0
Darien Payton <darien.payton71@gmail.com>
2019-05-22 13:36:59 -0400

    added helper to find users by email

---
+++ helpers/userExists.js
@@ -0,0 +1,24 @@
+const db = require('../database/dbConfig');
+
+const userExists = async (email) => {
+    let output;
+    // formats added emails to be lowercase
+    let formattedEmail = email.toLowercase()
+
+    await db('users')
+        .where({email: formattedEmail})
+        .then(user => {
+            // denotes if email exists
+            if(user.length){
+                output = true
+            } else {
+                output = false
+            }
+        })
+        .catch(() => {
+            throw "System could not search for users"
+        })
+    return output;
+};
+
+modules.export= userExists;
