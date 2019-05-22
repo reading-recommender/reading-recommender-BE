153e27f46f0dea322026759d1f4cd226b604872c
Darien Payton <darien.payton71@gmail.com>
2019-05-22 14:37:29 -0400

    added comment

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile');
 
+// use environment key
 const dbEnv = process.env.DB_ENV || 'development';
 
 modules.export = knex(knexConfig[dbEnv]);
