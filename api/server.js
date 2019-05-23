fabcef591f1c498964db40c56451a8fc7cbba306
Darien Payton <darien.payton71@gmail.com>
2019-05-23 19:39:41 -0400

    updated server file to handle modules

--- api/server.js
+++ api/server.js
@@ -1,87 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 
-const db = require('../database/dbConfig');
-const getBookRecommendation = require('../helpers/getBookRecommendation');
+const server = express();
 
-const server = express()
+const userAuth = require('./authRouter');
+const bookRecommendation = require('./recommendationRouter')
 
 server.use(express.json());
 server.use(cors());
 
-server.post('/api/signup', (req, res) => {
-    const user = req.body
+server.use('/api', userAuth);
+server.use('/', bookRecommendation);
 
-    user.password = bcrypt.hashSync(user.password, 10)
+server.get('/', () => {
+  res.send('The server is working')
+});
 
-    db('users')
-    .insert(user)
-    .then(user => {
-        res.status(201).json({message: 'User is signed up!'})
-    })
-})
-
-const secret = 'remove this later'
-function generateToken(user) {
-    const payload = {
-        subject: user.id,
-        username: user.username,
-    }
-    const options = {
-        expiresIn: '1d'
-    }
-
-    return jwt.sign(payload, secret, options);
-}
-
-server.post('/api/login', (req, res) => {
-    const {username, password } = req.body
-
-    db('users') .first()
-    .where({username})
-    .then(user => {
-        if(user && bcrypt.compareSync(password, user.password)) {
-            const token = generateToken(user);
-
-            res.status(201).json({token, message: 'User is logged in!'})
-        } else {
-            res.status(401).send('Invalid credentials')
-        }
-    })
-})
-
-server.post("/recommend", async (req, res) => {
-    const {
-      Q1,
-      Q2,
-      Q3,
-      Q4,
-      Q5,
-      Q6
-    } = req.body;
-  
-    const answers = {
-      Q1,
-      Q2,
-      Q3,
-      Q4,
-      Q5,
-      Q6
-    };
-
-    const recommendation = await getBookRecommendation(answers);
-        try{
-            if (recommendation) {
-              res.status(200).json({ ...recommendation});
-            } else {
-              throw "Please answer all the questions";
-            }
-        } catch (error) {
-      res.status(500).json({
-        message: "Error recommending book"
-      });
-    }
-  });
 module.exports = server;
