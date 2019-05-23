79074b54b321d7fb293b25254d4ff966db4687e0
Darien Payton <darien.payton71@gmail.com>
2019-05-22 20:26:23 -0400

    updated database

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './database/usersDb.db3'
+      filename: './database/test.db3'
     },
     useNullAsDefault: true,
     migrations: {
