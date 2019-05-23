44bb6b5a0853283e1f274bc39b9e7139f4aed9a5
Darien Payton <darien.payton71@gmail.com>
2019-05-23 19:30:25 -0400

    created router for recommendation

---
+++ api/recommendationRouter.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const getBookRecommendation = require('../helpers/getBookRecommendation');
+
+const router = express.Router();
+
+router.post("/recommend", async (req, res) => {
+    const {
+      Q1,
+      Q2,
+      Q3,
+      Q4,
+      Q5,
+      Q6
+    } = req.body;
+  
+    const answers = {
+      Q1,
+      Q2,
+      Q3,
+      Q4,
+      Q5,
+      Q6
+    };
+
+    const recommendation = await getBookRecommendation(answers);
+        try{
+            if (recommendation) {
+              res.status(200).json({ ...recommendation});
+            } else {
+              throw "Please answer all the questions";
+            }
+        } catch (error) {
+      res.status(500).json({
+        message: "Error recommending book"
+      });
+    }
+});
+
+module.exports = router;
